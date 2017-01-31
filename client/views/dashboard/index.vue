1053c4da3413876a4b6e4715f8b4941fae4af548
Erickson Juang <a820224@gmail.com>
2017-01-31 08:39:51 +0800

    re-design dashboard

--- client/views/dashboard/index.vue
+++ client/views/dashboard/index.vue
@@ -3,114 +3,87 @@
     <div class="tile is-ancestor">
       <div class="tile is-parent">
         <article class="tile is-child box">
-          <p class="title">One</p>
-          <p class="subtitle">Subtitle</p>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child box">
-          <p class="title">Two</p>
-          <p class="subtitle">Subtitle</p>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child box">
-          <p class="title">Three</p>
-          <p class="subtitle">Subtitle</p>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child box">
-          <p class="title">Four</p>
-          <p class="subtitle">Subtitle</p>
-        </article>
-      </div>
-    </div>
-
-    <div class="tile is-ancestor">
-      <div class="tile is-parent is-6">
-        <article class="tile is-child box">
-          <h4 class="title">Five</h4>
-          <div class="content">
-            <chart :type="'doughnut'" :data="chartData"></chart>
-          </div>
-        </article>
-      </div>
-      <div class="tile is-parent is-6">
-        <article class="tile is-child box">
-          <h4 class="title">Six</h4>
-          <div class="content">
-            <chart :type="'pie'" :data="chartData"></chart>
+          <h1 class="title">App 使用率</h1>
+          <div class="block styles-box">
+            <progress-bar :type="'info'" :value="45" :max="100" :show-label="true"></progress-bar>
           </div>
         </article>
       </div>
     </div>
 
     <div class="tile is-ancestor">
-      <div class="tile is-vertical is-9">
-        <div class="tile">
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">Seven</p>
-              <p class="subtitle">Subtitle</p>
-              <div class="content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-              </div>
-            </article>
-          </div>
-          <div class="tile is-8 is-parent">
-            <article class="tile is-child box">
-              <p class="title">Eight</p>
-              <p class="subtitle">Subtitle</p>
-              <div class="content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-              </div>
-            </article>
-          </div>
-        </div>
-        <div class="tile">
-          <div class="tile is-8 is-parent">
-            <article class="tile is-child box">
-              <p class="title">Nine</p>
-              <p class="subtitle">Subtitle</p>
-              <div class="content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-              </div>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">Ten</p>
-              <p class="subtitle">Subtitle</p>
-              <div class="content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-              </div>
-            </article>
-          </div>
-        </div>
-      </div>
-      <div class="tile is-parent">
+      <div class="tile is-parent is-3">
         <article class="tile is-child box">
+          <h4 class="title">會眾報到</h4>
           <div class="content">
-            <p class="title">Eleven</p>
-            <p class="subtitle">Subtitle</p>
-            <div class="content">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
-              <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
-            </div>
+            <chart :type="'doughnut'" :data="CIData.attendees"></chart>
+          </div>
+        </article>
+      </div>
+      <div class="tile is-parent is-3">
+        <article class="tile is-child box">
+          <h4 class="title">講者報到</h4>
+          <div class="content">
+            <chart :type="'doughnut'" :data="CIData.speaker"></chart>
+          </div>
+        </article>
+      </div>
+      <div class="tile is-parent is-3">
+        <article class="tile is-child box">
+          <h4 class="title">工人報到</h4>
+          <div class="content">
+            <chart :type="'doughnut'" :data="CIData.staff"></chart>
+          </div>
+        </article>
+      </div>
+      <div class="tile is-parent is-3">
+        <article class="tile is-child box">
+          <h4 class="title">迎賓袋領取</h4>
+          <div class="content">
+            <chart :type="'doughnut'" :data="kitData"></chart>
           </div>
         </article>
       </div>
     </div>
+
+    <div class="tile is-ancestor">
+      <div class="tile is-parent is-4">
+        <article class="tile is-child box">
+          <h4 class="title">午餐葷素分佈</h4>
+          <div class="content">
+            <chart :type="'doughnut'" :data="kitData"></chart>
+          </div>
+        </article>
+      </div>
+      <div class="tile is-parent is-4">
+        <article class="tile is-child box">
+          <h4 class="title">午餐發放狀況（葷食）</h4>
+          <div class="content">
+            <chart :type="'doughnut'" :data="kitData"></chart>
+          </div>
+        </article>
+      </div>
+      <div class="tile is-parent is-4">
+        <article class="tile is-child box">
+          <h4 class="title">午餐發放狀況（素食）</h4>
+          <div class="content">
+            <chart :type="'doughnut'" :data="kitData"></chart>
+          </div>
+        </article>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 import Chart from 'vue-bulma-chartjs'
+import ProgressBar from '../modules/ProgressBar'
 
 export default {
   components: {
-    Chart
+    Chart,
+    ProgressBar
   },
 
   data() {
@@ -120,19 +93,68 @@ export default {
   },
 
   computed: {
-    chartData() {
+    CIData() {
+      return {
+        attendees: {
+          labels: [
+            'App報到',
+            '人工報到',
+            '未報到'
+          ],
+          datasets: [{
+            data: this.data,
+            backgroundColor: [
+              '#36A2EB',
+              '#FFCE56',
+              '#FF6384'
+            ]
+          }]
+        },
+        staff: {
+          labels: [
+            'App報到',
+            '人工報到',
+            '未報到'
+          ],
+          datasets: [{
+            data: this.data,
+            backgroundColor: [
+              '#36A2EB',
+              '#FFCE56',
+              '#FF6384'
+            ]
+          }]
+        },
+        speaker: {
+          labels: [
+            'App報到',
+            '人工報到',
+            '未報到'
+          ],
+          datasets: [{
+            data: this.data,
+            backgroundColor: [
+              '#36A2EB',
+              '#FFCE56',
+              '#FF6384'
+            ]
+          }]
+        }
+      }
+    },
+    kitData() {
       return {
         labels: [
-          'Red',
-          'Blue',
-          'Yellow'
+          '已領取',
+          '報到但尚未領取',
+          '未報到'
         ],
         datasets: [{
           data: this.data,
           backgroundColor: [
-            '#FF6384',
             '#36A2EB',
-            '#FFCE56'
+            '#FFCE56',
+            '#FF6384'
           ]
         }]
       }
@@ -140,13 +162,25 @@ export default {
   },
 
   mounted() {
-    setInterval(() => {
-      // https://github.com/vuejs/vue/issues/2873
-      // Array.prototype.$set/$remove deprecated (use Vue.set or Array.prototype.splice instead)
-      this.data.forEach((item, i) => {
-        this.data.splice(i, 1, Math.ceil(Math.random() * 1000))
-      })
-    }, 1024)
+    // setInterval(() => {
+    //   // https://github.com/vuejs/vue/issues/2873
+    //   // Array.prototype.$set/$remove deprecated (use Vue.set or Array.prototype.splice instead)
+    //   this.data.forEach((item, i) => {
+    //     this.data.splice(i, 1, Math.ceil(Math.random() * 1000))
+    //   })
+    // }, 1024)
+    this.$http.get('//' + this.$store.state.api + '/dashboard').then((res) => {
+      console.log('successful')
+      console.log(res.body)
+      this.dashboard = res.body
+    }, (err) => {
+      console.error(err)
+      if (err.status === 401) {
+        window.location = '/'
+      }
+    })
+
+    console.log(this.kitData)
   }
 }
 </script>
---
+++ client/views/modules/ProgressBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="progress-container">
+    <div class="has-text-centered" v-if="showLabel">{{value}} / {{max}} - {{percentage + '%'}}</div>
+    <progress :class="['progress', type ? `is-${type}` : '', size ? `is-${size}` : '']" :value="value" :max="max"></progress>
+  </div>
+</template>
+
+<script>
+/*
+  Modify from https://raw.githubusercontent.com/vue-bulma/progress-bar/master/src/ProgressBar.vue
+ */
+export default {
+  props: {
+    type: String,
+    size: String,
+    value: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    max: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    showLabel: Boolean
+  },
+
+  computed: {
+    percentage() {
+      return this.value / this.max * 100
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+.progress-container {
+  align-items: center;
+  margin-bottom: 20px;
+
+  .progress {
+    position: relative;
+    margin-bottom: 0 !important;
+  }
+
+  .progress + span {
+    margin-left: 10px;
+    min-width: 36px;
+    text-align: right;
+  }
+}
+</style>
