8dc8c3f29bc22744726cec65321d8026d25318cb
Erickson Juang <a820224@gmail.com>
2017-01-31 08:39:16 +0800

    add login form

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="content has-text-centered">
-    <p>
-      <img width="200" src="~assets/logo.svg" :alt="description">
-    </p>
-
-    <h1 class="is-title is-bold">{{ name.replace('-', ' ') }}</h1>
-
-    <p>
-      <strong>{{ description }}</strong>,
-      <a :href="homepage">Live Demo</a>
-    </p>
-
-    <p>Supports Vue 2.0 and Bulma 0.2!</p>
+  <div class="content">
+    <div class="tile is-ancestor">
+      <div class="tile is-parent is-desktop is-4 is-offset-4 column box">
+        <div class="tile is-child">
+          <h1 class="title has-text-centered">Login</h1>
+          <label class="label">Username</label>
+          <p class="control has-icon has-icon-right">
+            <input class="input" type="text" placeholder="Your username">
+          </p>
+          <label class="label">Password</label>
+          <p class="control has-icon has-icon-right">
+            <input class="input" type="password" placeholder="Your password">
+          </p>
+          <div class="right-aligned">
+            <button class="button is-primary is-medium">Login</button>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,5 +35,8 @@ export default {
 <style lang="scss" scoped>
 .is-title {
   text-transform: capitalize;
+}
+.right-aligned {
+  justify-content: flex-end;
 }
 </style>
