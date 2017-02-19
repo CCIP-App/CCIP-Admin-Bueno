2a942e98a94c195210971ad0385796bd1330a8da
Erickson Juang <a820224@gmail.com>
2017-02-19 09:33:21 +0800

    Complete dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,35 +6,54 @@
           <v-card>
             <v-card-text>
               <h4 class="ma-0 text-xs-left">App 使用率</h4>
-              <p class="text-xs-center ma-0 mt-4">{{appUse+" / 100 - "+appUse+"%"}}</p>
-              <v-progress-linear v-model="appUse" class="ma-0 mb-4"></v-progress-linear>
+              <p class="text-xs-center ma-0 mt-4">{{ appLogged + " / " + totalUser + " - " + appPrecnetage + "%"}}</p>
+              <v-progress-linear v-model="appPrecnetage" class="ma-0 mb-4"></v-progress-linear>
             </v-card-text>
           </v-card>
         </v-col>
       </v-row>
-      <br>
-      <v-row>
-        <v-col xs12 md4>
+      <v-row class="mt-3">
+        <v-col xs12 md12>
           <v-card>
             <v-card-text>
-              <h4 class="ma-0 text-xs-left">App 使用率</h4>
-              <high-chart :options="defaultOption(CIData.attendees)" style="display: flex" idName="1"></high-chart>
+              <h4 class="ma-0 text-xs-left">即時報到率</h4>
+              <high-chart :options="defaultOption(attendees)" style="display: flex" idName="1"></high-chart>
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col xs12 md4>
+      </v-row>
+      <v-row class="mt-3">
+        <v-col xs12 md6>
           <v-card>
             <v-card-text>
-              <h4 class="ma-0 text-xs-left">App 使用率</h4>
-              <high-chart :options="defaultOption(CIData.attendees)" style="display: flex" idName="2"></high-chart>
+              <h4 class="ma-0 text-xs-left">午餐(葷)已報到 {{ lunch.meat.total }}</h4>
+              <high-chart :options="defaultOption(lunch.meat.chart)" style="display: flex" idName="2"></high-chart>
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col xs12 md4>
+        <v-col xs12 md6>
           <v-card>
             <v-card-text>
-              <h4 class="ma-0 text-xs-left">App 使用率</h4>
-              <high-chart :options="defaultOption(CIData.attendees)" style="display: flex" idName="3"></high-chart>
+              <h4 class="ma-0 text-xs-left">午餐(素)已報到 {{ lunch.vegetarian.total }}</h4>
+              <high-chart :options="defaultOption(lunch.vegetarian.chart)" style="display: flex" idName="3"></high-chart>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row class="mt-3">
+        <v-col xs12 md6>
+          <v-card>
+            <v-card-text>
+              <h4 class="ma-0 text-xs-left">迎賓袋領取 {{ totalUser }}</h4>
+              <high-chart :options="defaultOption(kit.normal.chart)" style="display: flex" idName="4"></high-chart>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col xs12 md6>
+          <v-card>
+            <v-card-text>
+              <h4 class="ma-0 text-xs-left">個人贊助領取 {{ kit.vip.total }}</h4>
+              <high-chart :options="defaultOption(kit.vip.chart)" style="display: flex" idName="5"></high-chart>
             </v-card-text>
           </v-card>
         </v-col>
@@ -44,30 +63,109 @@
 </template>
 
 <script>
+  import apiClient from '../modal/apiClient'
   export default {
     name: 'Dashboard',
     data() {
       return {
-        appUse: 45
+        CIData: {
+          logged: 0,
+          total: 0,
+          day1checkin_used: 0,
+          day1lunch: {
+            meat: 0,
+            meat_used: 0,
+            total: 0,
+            vegetarian: 0,
+            vegetarian_used: 0
+          },
+          kit_used: 0,
+          vipkit: {
+            total: 3,
+            used: 0
+          }
+        }
       }
     },
     computed: {
-      CIData() {
+      appLogged() {
+        return this.CIData.logged
+      },
+      totalUser() {
+        return this.CIData.total
+      },
+      appPrecnetage() {
+        return Math.round(this.appLogged / this.totalUser * 100)
+      },
+      attendees() {
+        return [
+          {
+            name: '已報到',
+            y: this.CIData.day1checkin_used
+          },
+          {
+            name: '未報到',
+            y: this.CIData.total - this.CIData.day1checkin_used
+          }
+        ]
+      },
+      lunch() {
         return {
-          attendees: [
-            {
-              name: 'App報到',
-              y: 300
-            },
-            {
-              name: '人工報到',
-              y: 50
-            },
-            {
-              name: '未報到',
-              y: 100
-            }
-          ]
+          meat: {
+            total: this.CIData.day1lunch.meat,
+            chart: [
+              {
+                name: '葷食已領取',
+                y: this.CIData.day1lunch.meat_used
+              },
+              {
+                name: '葷食未領取',
+                y: this.CIData.day1lunch.meat - this.CIData.day1lunch.meat_used
+              }
+            ]
+          },
+          vegetarian: {
+            total: this.CIData.day1lunch.vegetarian,
+            chart: [
+              {
+                name: '素食已領取',
+                y: this.CIData.day1lunch.vegetarian_used
+              },
+              {
+                name: '素食未領取',
+                y: this.CIData.day1lunch.vegetarian - this.CIData.day1lunch.vegetarian_used
+              }
+            ]
+          }
+        }
+      },
+      kit() {
+        return {
+          normal: {
+            chart: [
+              {
+                name: '已領取',
+                y: this.CIData.kit_used
+              },
+              {
+                name: '未領取',
+                y: this.totalUser - this.CIData.kit_used
+              }
+            ]
+          },
+          vip: {
+            total: this.CIData.vipkit.total,
+            chart: [
+              {
+                name: '已領取',
+                y: this.CIData.vipkit.used
+              },
+              {
+                name: '未領取',
+                y: this.CIData.vipkit.total - this.CIData.vipkit.used
+              }
+            ]
+          }
         }
       }
     },
@@ -87,7 +185,7 @@
             series: {
               dataLabels: {
                 enabled: true,
-                format: '{point.name}<br> {point.y:.1f}%',
+                format: '{point.name}<br> {point.y}',
                 distance: -30
               }
             }
@@ -106,6 +204,12 @@
       }
     },
     mounted() {
+      apiClient.getDasboard()
+        .then((res) => {
+          this.CIData = res.data
+        }, (err) => {
+          console.error(err)
+        })
     }
   }
 </script>
