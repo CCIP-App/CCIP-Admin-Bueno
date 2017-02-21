b8d53016ca17fa319ab11214a3ada3b11f6d64d8
Erickson Juang <a820224@gmail.com>
2017-02-21 21:45:54 +0800

    Clear code
    1. Fix coding style
    2. Remove qrcode-reader

--- src/components/QrcodeReader.vue
+++ src/components/QrcodeReader.vue
@@ -11,100 +11,97 @@
 </template>
 
 <script>
-  import Qrcode from 'qrcode-reader'
-  var qr = new Qrcode()
-  export default {
-    name: 'QrcodeReader',
-    props: {
-      title: {
-        type: String,
-        default: ''
-      },
-      subTitle: {
-        type: String,
-        default: ''
-      },
-      enable: Boolean,
-      noResult: Boolean,
-      width: {
-        type: String,
-        default: 320 + 'px'
-      },
-      height: {
-        type: String,
-        default: 240 + 'px'
-      }
+export default {
+  name: 'QrcodeReader',
+  props: {
+    title: {
+      type: String,
+      default: ''
     },
-    data() {
-      return {
-        result: 'Loading...',
-        cam: null,
-        webrtc: true,
-        scanner: null
-      }
+    subTitle: {
+      type: String,
+      default: ''
     },
-    watch: {
-      enable: function (state) {
-        self.scanner.setStopped(!state);
-      }
+    enable: Boolean,
+    noResult: Boolean,
+    width: {
+      type: String,
+      default: 320 + 'px'
     },
-    mounted() {
-      var self = this
-      window.w69b.qr.decoding.setWorkerUrl('public/barcode.js/w69b.qrcode.decodeworker.min.js');
-      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
-      if (navigator.getUserMedia) {
-        self.webrtc = true
-        self.scanner = new window.w69b.qr.ui.ContinuousScanner();
-        self.scanner.setDecodedCallback(function (result) {
-          self.onSuccess(result)
-        });
-        self.scanner.render(document.getElementById('camsource'));
-      } else {
-        self.webrtc = false
-        console.log('Sorry, native web camera streaming (getUserMedia) is not supported by this browser...')
-      }
-
-    },
-    beforeDestroy() {
-      var self = this
-      self.scanner.setStopped(true);
-      self.scanner.dispose();
-    },
-    methods: {
-
-      onSuccess(result) {
-        this.result = result
-        this.$emit('OnSuccess', result)
-      },
-      uploadChange() {
-        var self = this
-        var file = document.getElementById('upload').files[0]
-        var imageType = /^image\//
-
-        if (!imageType.test(file.type)) {
-          console.log('File type not valid')
-        }
-        // Read file
-        var reader = new FileReader()
-        reader.addEventListener('load', function () {
-          var image = new Image()
-          image.onload = function (imageEvent) {
-            // Resize the image
-            var decoder = new window.w69b.qr.decoding.Decoder()
-            decoder.decode(image).then(function (result) {
-              // succesfully decoded QR Code.
-              self.onSuccess(result.text);
-            }, function () {
-              self.$emit('OnError', 'no qr code found')
-            });
-          }
-          image.src = this.result
-        }.bind(reader), false)
-        reader.readAsDataURL(file)
-      }
-
+    height: {
+      type: String,
+      default: 240 + 'px'
     }
+  },
+  data() {
+    return {
+      result: 'Loading...',
+      cam: null,
+      webrtc: true,
+      scanner: null
+    }
+  },
+  watch: {
+    enable: function(state) {
+      self.scanner.setStopped(!state)
+    }
+  },
+  mounted() {
+    var self = this
+    window.w69b.qr.decoding.setWorkerUrl('public/barcode.js/w69b.qrcode.decodeworker.min.js')
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
+    if (navigator.getUserMedia) {
+      self.webrtc = true
+      self.scanner = new window.w69b.qr.ui.ContinuousScanner()
+      self.scanner.setDecodedCallback(function(result) {
+        self.onSuccess(result)
+      })
+      self.scanner.render(document.getElementById('camsource'))
+    } else {
+      self.webrtc = false
+      console.log('Sorry, native web camera streaming (getUserMedia) is not supported by this browser...')
+    }
+  },
+  beforeDestroy() {
+    var self = this
+    self.scanner.setStopped(true)
+    self.scanner.dispose()
+  },
+  methods: {
+
+    onSuccess(result) {
+      this.result = result
+      this.$emit('OnSuccess', result)
+    },
+    uploadChange() {
+      var self = this
+      var file = document.getElementById('upload').files[0]
+      var imageType = /^image\//
+
+      if (!imageType.test(file.type)) {
+        console.log('File type not valid')
+      }
+        // Read file
+      var reader = new FileReader()
+      reader.addEventListener('load', function() {
+        var image = new Image()
+        image.onload = function(imageEvent) {
+            // Resize the image
+          var decoder = new window.w69b.qr.decoding.Decoder()
+          decoder.decode(image).then(function(result) {
+              // succesfully decoded QR Code.
+            self.onSuccess(result.text)
+          }, function() {
+            self.$emit('OnError', 'no qr code found')
+          })
+        }
+        image.src = this.result
+      }.bind(reader), false)
+      reader.readAsDataURL(file)
+    }
+
   }
+}
 
 </script>
 
