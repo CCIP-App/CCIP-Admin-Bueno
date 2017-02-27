3cc9a5cdd80cba1cd1d47aeb134fb47b572e364d
Erickson Juang <a820224@gmail.com>
2017-02-28 05:33:23 +0800

    Updating api

--- src/modal/apiClient.js
+++ src/modal/apiClient.js
@@ -12,16 +12,21 @@ const client = axios.create(config)
 
 export default {
   getDasboard: () => {
-    console.log('client', client)
     return client.get('dashboard')
   },
+  checkIn: (token) => {
+    return client.get('use/day1checkin?token=' + token + '&StaffQuery=true').then((res) => res.data)
+  },
   getStatus: (token) => {
-    return client.get('status?token=' + token)
+    return client.get('status?token=' + token).then((res) => res.data)
   },
   getNickname: (token) => {
-    return client.get('landing?token=' + token)
+    return client.get('landing?token=' + token).then((res) => res.data.nickname)
   },
   getPuzzle: (pubToken) => {
-    return client.get('event/puzzle?token=' + pubToken)
+    return client.get('event/puzzle?token=' + pubToken).then((res) => res.data)
+  },
+  revokPlayer: (token) => {
+    return client.get('event/puzzle/revoke?token=' + token).then((res) => ({token: token, successful: res.data.status === 'OK'}))
   }
 }
