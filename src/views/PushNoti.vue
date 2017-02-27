290df0fd1f42c3c63e158837470054214b2254c2
Erickson Juang <a820224@gmail.com>
2017-02-28 07:46:09 +0800

    Implement PushNoti

--- src/views/PushNoti.vue
+++ src/views/PushNoti.vue
@@ -2,55 +2,85 @@
   <div id='PushNoti'>
     <v-container fluid>
       <v-row>
-        <v-col xs12 md6 style="margin: 0 auto;">
+        <v-col xs12 md12 style="margin: 0 auto;">
           <v-card style="margin: 0 auto;">
             <v-card-text class=" text-xs-center">
               <h5 class="ma-0">新增推播通知</h5>
-              <v-select v-bind:options="options" placeholder="選擇對象" v-model.number="addPush.to" :disabled="disabled"></v-select>
-              <v-text-input type="text" placeholder="Chinese" v-model="addPush.zh" :disabled="disabled"></v-text-input>
-              <v-text-input type="text" placeholder="English" v-model="addPush.en" :disabled="disabled"></v-text-input>
-              <v-text-input type="text" placeholder="URI(option)" v-model="addPush.uri" :disabled="disabled"></v-text-input>
+              <v-select v-bind:options="options" placeholder="選擇對象" v-model.number="feed.to" :disabled="disabled"></v-select>
+              <v-text-input type="text" placeholder="Msg" v-model="feed.msg" :disabled="disabled"></v-text-input>
+              <v-text-input type="text" placeholder="URI(option)" v-model="feed.uri" :disabled="disabled"></v-text-input>
               <v-btn ripple info @click.native="send" :disabled="disabled" :loading="disabled">Send!</v-btn>
             </v-card-text>
           </v-card>
         </v-col>
       </v-row>
+      <v-alert dismissible warning v-model="alert" role="alert">{{ alertMessage }}</v-alert>
+      <v-alert dismissible success v-model="success" role="alert">推播成功</v-alert>
+
     </v-container>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'PushNoti',
-    data() {
-      return {
-        options:[{
-          value:1,
-          text:"全體"
-        },{
-          value:2,
-          text:"僅會眾"
+import oneSignal from '../modal/onesignal'
+export default {
+  name: 'PushNoti',
+  data() {
+    return {
+      options: [
+        {
+          value: 'All',
+          text: '全體'
+        }, {
+          value: 'Audience',
+          text: '僅會眾'
         },
         {
-          value:3,
-          text:"僅工作人員"
+          value: 'Staff',
+          text: '僅工作人員'
         },
-        ],
-        addPush: {
-          to: 0,
-          zh: "",
-          en: "",
-          uri: ""
-        },
-        disabled: false
+        {
+          value: 'Speaker',
+          text: '僅講者'
+        }
+      ],
+      feed: {
+        to: 0,
+        msg: '',
+        uri: ''
+      },
+      disabled: false,
+      alert: false,
+      success: false,
+      alertMessage: ''
+    }
+  },
+  methods: {
+    send() {
+      this.alert = false
+      this.success = false
+      let packet = {
+        target: [this.feed.to],
+        en: this.feed.msg
       }
-    },
-    methods: {
-      send(){
-
+      if (this.feed.uri.length > 0) packet['uri'] = this.feed.uri
+      if (this.feed.msg.length > 0 && this.feed.to !== 0) {
+        oneSignal.createNotification(packet)
+          .then((res) => {
+            this.success = true
+          })
+          .catch((err) => {
+            console.log(err)
+            this.alertMessage = 'Something error on network'
+            this.alert = true
+          })
+      } else {
+        this.alertMessage = '欄位不完整'
+        this.alert = true
       }
     }
   }
+}
 
 </script>
 
