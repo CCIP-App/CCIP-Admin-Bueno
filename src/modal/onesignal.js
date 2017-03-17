3b8dccd7bd6e90dd7f6a935e816babe9e5b7e42e
Erickson Juang <a820224@gmail.com>
2017-03-17 16:57:02 +0800

    Fix #1

--- src/modal/onesignal.js
+++ src/modal/onesignal.js
@@ -22,7 +22,9 @@ export default {
       'app_id': oneSignalConfig.app_id,
       'included_segments': packet.target,
       'contents': {
-        'en': packet.en
+        'en': packet.en,
+        'zh-Hant': packet.en,
+        'zh-Hans': packet.en
       },
       'url': packet.uri
     })
