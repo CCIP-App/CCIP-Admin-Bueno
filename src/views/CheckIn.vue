61a2cc1a39a4d602d8991853faa181e449da7cbc
腹黒い茶 <haraguroicha@tipsy.coffee>
2019-08-22 16:08:56 +0800

    freshen checkin UI to friendly for mobile user

--- src/views/CheckIn.vue
+++ src/views/CheckIn.vue
@@ -1,7 +1,7 @@
 <template>
   <div id='CheckIn'>
-    <v-alert dismissible type="warning" v-model="alert" role="alert" class="mb-3">{{ alertMessage }}</v-alert>
-    <v-alert dismissible type="success" v-model="successCI" role="alert" class="mb-3">{{ alertMessage }}</v-alert>
+    <v-alert dismissible type="warning" v-model="alert" prominent role="alert" class="pa-3">{{ alertMessage }}</v-alert>
+    <v-alert dismissible type="success" v-model="successCI" role="alert" class="pa-3">{{ alertMessage }}</v-alert>
     <v-select
       :items="checkInItems"
       label="選擇報到方法"
@@ -14,30 +14,22 @@
       </v-flex>
       <v-flex xs12 md6>
         <v-card>
-          <!-- <v-card-row class="green darken-1"> -->
-            <v-card-title>
-            <span class="white--text">CheckIn</span>
-            </v-card-title>
-          <!-- </v-card-row> -->
           <v-card-text>
-            <!-- <v-card-row> -->
-              <ul v-if="user.user_id" role="userStatus">
-                <li>Nickname: <span class="user_info">{{ user.user_id }}</span></li>
-                <li>App login: <span class="user_info">{{ user.first_use ? user.first_use : 'Not yet' }}</span></li>
-                <li>User role: <span class="user_info">{{ user.role }}</span></li>
-                <template v-for="(scenarios, index) in sortScenarios(this.scenario, user.scenarios)">
-                  <li :key="index">
-                    {{ scenarios.key }}: <span class="user_info">{{ (scenarios.used ? scenarios.used : 'Not yet') }}</span>
-                    <ul v-if="Object.keys(scenarios.attr).length > 0">
-                      <li v-for="(key, index) in Object.keys(scenarios.attr)" :key="index">
-                        {{ key }}: <span class="user_info">{{ scenarios.attr[key] }}</span>
-                      </li>
-                    </ul>
-                  </li>
-                  <hr v-if="index === 0" :key="'hr_'+index" class="datahr" >
-                </template>
-              </ul>
-            <!-- </v-card-row> -->
+            <ul v-if="user.user_id" role="userStatus">
+              <li>Nickname(Role): <span class="user_info">{{ user.user_id }} ({{ user.role }})</span></li>
+              <li>App login: <span class="user_info">{{ user.first_use ? user.first_use : 'Not yet' }}</span></li>
+              <template v-for="(scenarios, index) in sortScenarios(this.scenario, user.scenarios)">
+                <li :key="index">
+                  {{ scenarios.key }}: <span class="user_info">{{ (scenarios.used ? scenarios.used : 'Not yet') }}</span>
+                  <ul v-if="Object.keys(scenarios.attr).length > 0">
+                    <li v-for="(key, index) in Object.keys(scenarios.attr)" :key="index">
+                      {{ key }}: <span class="user_info">{{ scenarios.attr[key] }}</span>
+                    </li>
+                  </ul>
+                </li>
+                <hr v-if="index === 0" :key="'hr_'+index" class="datahr" >
+              </template>
+            </ul>
           </v-card-text>
         </v-card>
       </v-flex>
@@ -114,9 +106,9 @@ export default {
       }).catch((err) => {
         if (err.response) {
           if ('link expired/not available now'.match(err.response.data.message)) {
-            this.alertMessage = '還沒開始報到喔(╯°□°）╯︵ ┻━┻'
+            this.alertMessage = '還沒開始'
           } else if ('has been used'.match(err.response.data.message)) {
-            this.alertMessage = '已經報到過了╮(￣▽￣"")╭'
+            this.alertMessage = '已經報到過了'
           } else {
             this.alertMessage = err.response.status + ' - ' + err.response.data.message
           }
